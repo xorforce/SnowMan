7c5142bb27ab5b6a9b62bdd99576557e0411f5b0
xorforce <bhagatsingh2297@gmail.com>
2018-03-05 22:39:08 +0530

    Initial Commit

---
+++ SnowMan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnowMan
+//
+//  Created by Bhagat  Singh on 05/03/18.
+//  Copyright © 2018 Bhagat Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
